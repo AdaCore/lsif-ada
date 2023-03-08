e53c6fe00d551ddd0c75212a6abe58f68362035a
Vadim Godunko <godunko@adacore.com>
2023-03-08 18:30:49 +0400

    Add support of Excluded_Project_Files attribute.

--- source/lsif-projects.adb
+++ source/lsif-projects.adb
@@ -18,10 +18,11 @@
 with Ada.Text_IO;
 
 with VSS.Application;
+with VSS.Command_Line;
 with VSS.Strings.Conversions;
 
 with GNATCOLL.VFS;
-with GPR2.Path_Name;
+with GPR2.Path_Name.Set;
 with GPR2.Project.Attribute;
 with GPR2.Project.Registry.Attribute;
 with GPR2.Project.Registry.Pack;
@@ -33,32 +34,39 @@ with LSIF.Configuration;
 
 package body LSIF.Projects is
 
+   use type GNATCOLL.VFS.Virtual_File;
+
    procedure Register_Attributes;
    --  Register project file's attributes.
 
-   LSIF_Package             : constant GPR2.Package_Id :=
+   LSIF_Package                     : constant GPR2.Package_Id :=
      GPR2."+" ("lsif");
 
-   Workspace_Root_Attribute : constant GPR2.Attribute_Id :=
+   Excluded_Project_Files_Attribute : constant GPR2.Attribute_Id :=
+     GPR2."+" ("excluded_project_files");
+   Workspace_Root_Attribute         : constant GPR2.Attribute_Id :=
      GPR2."+" ("workspace_root");
 
-   LSIF_Workspace_Root      : constant GPR2.Q_Attribute_Id :=
+   LSIF_Workspace_Root              : constant GPR2.Q_Attribute_Id :=
      (LSIF_Package, Workspace_Root_Attribute);
+   LSIF_Excluded_Project_Files      : constant GPR2.Q_Attribute_Id :=
+     (LSIF_Package, Excluded_Project_Files_Attribute);
 
-   Project_Tree : GPR2.Project.Tree.Object;
+   Project_Tree           : GPR2.Project.Tree.Object;
+   Excluded_Project_Files : GPR2.Path_Name.Set.Object;
 
    ------------------------
    -- Build_Set_Of_Files --
    ------------------------
 
    procedure Build_Set_Of_Files is
-      use type GNATCOLL.VFS.Virtual_File;
-
    begin
       --  Prepare list of source files
 
       for View of Project_Tree loop
-         if not View.Is_Externally_Built then
+         if not View.Is_Externally_Built
+           and then not Excluded_Project_Files.Contains (View.Path_Name)
+         then
             for Source of View.Sources loop
                if Source.Is_Ada
                  and then
@@ -138,6 +146,36 @@ package body LSIF.Projects is
             LSIF.Configuration.Workspace_Root.Normalize_Path;
          end;
       end if;
+
+      --  Setup list of excluded project files
+
+      if Project_Tree.Root_Project.Has_Attribute
+        (LSIF_Excluded_Project_Files)
+      then
+         declare
+            Attribute : constant GPR2.Project.Attribute.Object :=
+              Project_Tree.Root_Project.Attribute (LSIF_Excluded_Project_Files);
+
+         begin
+            for Item of Attribute.Values loop
+               if Item.Text'Length = 0 then
+                  VSS.Command_Line.Report_Error
+                    ("empty name of the project file");
+               end if;
+
+               if Project_Tree.Get_File
+                    (GPR2.Filename_Type
+                       (Item.Text)).Virtual_File = GNATCOLL.VFS.No_File
+               then
+                  VSS.Command_Line.Report_Error
+                    ("unable to resolve project file path");
+               end if;
+
+               Excluded_Project_Files.Append
+                 (Project_Tree.Get_File (GPR2.Filename_Type (Item.Text)));
+            end loop;
+         end;
+      end if;
    end Initialize;
 
    -------------------------
@@ -149,6 +187,12 @@ package body LSIF.Projects is
       GPR2.Project.Registry.Pack.Add
         (LSIF_Package, GPR2.Project.Registry.Pack.Everywhere);
 
+      GPR2.Project.Registry.Attribute.Add
+        (Name                 => LSIF_Excluded_Project_Files,
+         Index_Type           => GPR2.Project.Registry.Attribute.No_Index,
+         Value                => GPR2.Project.Registry.Attribute.List,
+         Value_Case_Sensitive => True,
+         Is_Allowed_In        => GPR2.Project.Registry.Attribute.Everywhere);
       GPR2.Project.Registry.Attribute.Add
         (Name                 => LSIF_Workspace_Root,
          Index_Type           => GPR2.Project.Registry.Attribute.No_Index,
